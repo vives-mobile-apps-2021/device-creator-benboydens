7a32367ec77a6a5967861fbeed7124258c853789
Ben Boydens <ben.boydens@student.vives.be>
2021-11-11 20:46:56 +0100

    update wirefram

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
+    <v-app-bar app color="white" flat>
       <v-avatar
         :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
         size="32"
       ></v-avatar>
 
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
+      <v-tabs centered class="ml-n9" color="grey darken-1">
+        <v-tab v-for="link in links" :key="link">
           {{ link }}
         </v-tab>
       </v-tabs>
@@ -31,58 +20,22 @@
     </v-app-bar>
 
     <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-container>
+          <h1>Devices</h1>
+          <v-row>
+            <v-col v-for="n in 24" :key="n" cols="4">
+              <v-card height="200"></v-card>
+            </v-col>
+          </v-row>
+        </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    links: ["Home", "Devices", "Create"],
+  }),
+};
 </script>
