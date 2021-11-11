2a8e87a91cc5ee748eb3933dff5d78f0c0ecabda
Ben Boydens <ben.boydens@student.vives.be>
2021-11-11 21:11:19 +0100

    added vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,9 @@
 
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab v-for="link in links" :key="link">
-          {{ link }}
+          <router-link :to="link">
+            {{ link }}
+          </router-link>
         </v-tab>
       </v-tabs>
 
@@ -20,14 +22,14 @@
     </v-app-bar>
 
     <v-main class="grey lighten-3">
-        <v-container>
-          <h1>Devices</h1>
-          <v-row>
-            <v-col v-for="n in 24" :key="n" cols="4">
-              <v-card height="200"></v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+      <v-container>
+        <h1>Test</h1>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
