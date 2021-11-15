66a8c60e5b3a6ed358c62071471be97f61d0e52b
Ben Boydens <ben.boydens@student.vives.be>
2021-11-15 12:02:02 +0100

    add qr scan with files

--- frontend/src/components/QRscanner.vue
+++ frontend/src/components/QRscanner.vue
@@ -2,41 +2,64 @@
   <div>
     <v-sheet>
       <v-container>
-        <qrcode-stream @decode="onDecode" class="qr-stream mt-3">
-          <v-progress-linear
-            v-if="!qrcodevalue"
-            indeterminate
-            color="blue"
-          ></v-progress-linear>
-        </qrcode-stream>
+        <v-tabs v-model="tab">
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab" vertical>
+          <v-tab-item transition="false">
+            <qrcode-stream @decode="onDecode" class="qr-stream mt-3">
+              <v-progress-linear
+                v-if="!qrcodevalue"
+                indeterminate
+                color="blue"
+              ></v-progress-linear>
+            </qrcode-stream>
 
-        <div v-if="!qrcodevalue" class="qr-subtitle pa-3">
-          Scanning for QR-codes...
-        </div>
-        <div v-else class="qr-subtitle pa-3">
-          <v-icon color="green darken-2"> mdi-check-bold </v-icon>
-          Found 
-          <a :href="qrcodevalue">
-            {{ qrcodevalue }}
-          </a>
-        </div>
+            <div v-if="!qrcodevalue" class="qr-subtitle pa-3">
+              Scanning for QR-codes...
+            </div>
+            <div v-else class="qr-subtitle pa-3">
+              <v-icon color="green darken-2"> mdi-check-bold </v-icon>
+              Found
+              <a :href="qrcodevalue">
+                {{ qrcodevalue }}
+              </a>
+            </div>
+          </v-tab-item>
+          <v-tab-item transition="false">
+            <h5>Drop file here</h5>
+            <qrcode-capture @decode="onDecode"></qrcode-capture>
+            <div class="pa-3" v-if="qrcodevalue">
+              <v-icon color="green darken-2"> mdi-check-bold </v-icon>
+              Found
+              <a :href="qrcodevalue">
+                {{ qrcodevalue }}
+              </a>
+            </div>
+          </v-tab-item>
+        </v-tabs-items>
       </v-container>
     </v-sheet>
   </div>
 </template>
 
 <script>
-import { QrcodeStream } from "vue-qrcode-reader";
+import { QrcodeStream, QrcodeCapture } from "vue-qrcode-reader";
 
 export default {
   name: "QrScanner",
   data: () => {
     return {
       qrcodevalue: undefined,
+      tab: null,
+      items: ["Use Camera", "Use File"],
     };
   },
   components: {
     QrcodeStream,
+    QrcodeCapture 
   },
   methods: {
     onDecode(decodedString) {
