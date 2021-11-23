9aa92dfc1f877361c0724fd3ed06c0eccacdc08e
Ben Boydens <ben.boydens@student.vives.be>
2021-11-23 22:02:54 +0100

    removed about page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
 
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab to="/">
@@ -19,11 +15,8 @@
         <v-tab to="/scan">
           Scan
         </v-tab>
-        <v-tab to="/about">
-          About
-        </v-tab>
       </v-tabs>
-
+      <v-btn text>Login</v-btn>
       <v-avatar
         class="hidden-sm-and-down"
         color="grey darken-1 shrink"
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -39,14 +39,6 @@ const routes = [
     path: '/devices/:id',
     name: 'Device',
     component: DeviceView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
