f6bcf1d4c5fb49ae06e47ae39c834934d4567114
Ben Boydens <ben.boydens@student.vives.be>
2021-11-29 12:43:31 +0100

    created profile avatar comp

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,19 +3,15 @@
     <v-app-bar app color="white" flat>
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab to="/"> Home </v-tab>
-          <v-tab to="/devices"> Devices </v-tab>
-          <v-tab v-if="is_logged_in" to="/map"> Map </v-tab>
-          <v-tab v-if="is_logged_in" to="/scan"> Scan </v-tab>
-          <v-tab v-if="!is_logged_in" to="/register">Register</v-tab>
+        <v-tab to="/devices"> Devices </v-tab>
+        <v-tab v-if="is_logged_in" to="/map"> Map </v-tab>
+        <v-tab v-if="is_logged_in" to="/scan"> Scan </v-tab>
+        <v-tab v-if="!is_logged_in" to="/register">Register</v-tab>
       </v-tabs>
 
       <v-btn v-if="is_logged_in" text @click="logout">Logout</v-btn>
       <login-alert v-else />
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ><span v-if="is_logged_in" class="white--text">{{ credentials }}</span></v-avatar>
+      <profile-avatar />
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -28,11 +24,12 @@
 
 <script>
 import LoginAlert from "@/components/LoginAlert.vue";
+import ProfileAvatar from "@/components/ProfileAvatar.vue"
 
 export default {
   name: "App",
   components: {
-    LoginAlert,
+    LoginAlert, ProfileAvatar
   },
   methods: {
     logout() {
@@ -42,9 +39,6 @@ export default {
   computed: {
     is_logged_in() {
       return this.$store.state.user;
-    },
-    credentials() {
-      return this.$store.getters.get_credentials;
     }
   },
 };
---
+++ frontend/src/components/ProfileAvatar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-tooltip bottom v-if="user">
+    <template v-slot:activator="{ on, attrs }">
+      <v-avatar
+        v-bind="attrs"
+        v-on="on"
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+        ><span v-if="user" class="white--text">{{ credentials }}</span>
+      </v-avatar>
+    </template>
+    <div>{{ user.firstname }} {{ user.lastname }}</div>
+    <div>{{ user.email }}</div>
+  </v-tooltip>
+  <v-avatar
+    v-else
+    class="hidden-sm-and-down"
+    color="grey darken-1 shrink"
+    size="32"
+  >
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  name: "ProfileAvatar",
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+    credentials() {
+      return this.$store.getters.get_credentials;
+    },
+  },
+};
+</script>
