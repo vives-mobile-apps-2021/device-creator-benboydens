b9b7334f3273616d64356a98f9f673ea2a7b2982
benboydens <r0857165@student.vives.be>
2021-12-11 18:32:59 +0100

    sanitize user in login

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -71,7 +71,7 @@ const userRoute = {
             req.login(user, (err) => {
                 if (err) return res.status(500).send({ message: "Login action failed" });
 
-                res.send(user); // TODO sanitize user
+                res.send(sanitize_user(user));
             })
         })(req, res)    // Middleware
     },
