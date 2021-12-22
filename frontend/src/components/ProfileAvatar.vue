c68b2015f9ec50a54fb017da8983de8c74ebe201
benboydens <r0857165@student.vives.be>
2021-12-22 22:00:00 +0100

    added image to avatar in navbar

--- frontend/src/components/ProfileAvatar.vue
+++ frontend/src/components/ProfileAvatar.vue
@@ -7,8 +7,9 @@
           v-on="on"
           class="hidden-sm-and-down"
           color="grey darken-1 shrink"
-          size="32"
-          ><span v-if="user" class="white--text">{{ credentials }}</span>
+          size="40"
+        >
+          <img :src="avatar_url + '/images/' + user.avatar " />
         </v-avatar>
       </router-link>
     </template>
@@ -27,6 +28,11 @@
 <script>
 export default {
   name: "ProfileAvatar",
+  data: () => {
+    return {
+      avatar_url: process.env.VUE_APP_API_URL + ':' + process.env.VUE_APP_API_PORT
+    }
+  },
   computed: {
     user() {
       return this.$store.state.user;
